04b96c511d66f029a2086b3c19b069f2e0a7f3ae
Mira <mirakubai@gmail.com>
2021-03-18 15:57:09 +0200

    Update task_2.py
    changed

--- hw_8/task_2.py
+++ hw_8/task_2.py
@@ -2,6 +2,6 @@ def divisors(number):
     number = int(number)
     res = []
     for i in range(1, number + 1):
-        if int(number) % int(i) == 0:
+        if number % i == 0:
             res.append(i)
     return res
