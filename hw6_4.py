df74f56e6f953f5f1a15fabe4c26144dc8256cfd
Mira <mirakubai@gmail.com>
2021-03-19 15:54:21 +0200

    Update hw6_4.py
    update

--- hw6_4.py
+++ hw6_4.py
@@ -7,9 +7,7 @@ while True:
             print('Please, input only numbers')
     if year % 4 != 0:
         print('Year is low, 365 days)')
-    elif year % 100 != 0:
-        print('Year is leap, 366 days')
-    elif year % 400 == 0:
+    elif year % 100 != 0 and year % 400 == 0:
         print('Year is leap, 366 days')
     else:
-        print('Year is low, 365 days)')
+        print('Year is low, 365 days')
