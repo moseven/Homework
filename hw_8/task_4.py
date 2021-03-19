adb550b348700fae6745ccae20582c537520e9cc
Mira <mirakubai@gmail.com>
2021-03-20 00:18:41 +0200

    Update task_4.py
    changed

--- hw_8/task_4.py
+++ hw_8/task_4.py
@@ -1,4 +1,3 @@
-import os
 from os import listdir
 from os import path
 
@@ -7,16 +6,21 @@ def my_listdir(dir_path, flag=True):
     contents = []
     folders = []
     files = []
+    
     for subj in listdir(dir_path):
         subj_path = dir_path + '\\' + subj
+    
         if path.isfile(subj_path):
             files.append(subj)
-        if path.isdir(subj_path):
+        elif path.isdir(subj_path):
             folders.append(subj)
-            contents.append(my_listdir(subj_path, flag=False))
+            for i in my_listdir(subj_path, flag=False):
+                contents.append(i)
+
+    contents.insert(0, (dir_path, folders, files))
+
     if flag:
-        contents.insert(0, (dir_path, folders, files))
         print(contents)
     else:
-        return dir_path, folders, files
+        return contents
     
